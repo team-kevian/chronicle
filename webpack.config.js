598ac341295634b2fc456a72df77da5aecf6a10c
kgxu <Kevinx88@gmail.com>
2019-06-26 16:10:59 -0400

    config webpack/babel and package.json, added Search component

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.eports = {
+  entry: `${SRC_DIR}/inde.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          preset: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
